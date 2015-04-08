4c3ffb82acb2d00667347789c8b881884a751226
Jovi Chokor <jomonavi@gmail.com>
2015-04-07 22:42:59 -0400

    Week 2 Commit

---
+++ game.js
@@ -0,0 +1,44 @@
+// I did this before i read the week 2 assignment about what functional programming is,
+//and i now realized i used a lot of functional programming in the game function.
+//I still need to use "jQuery-fy" the game and make the program pretty with css.  
+function numGenerator() {
+    return Math.floor(Math.random() * 101);
+}
+
+function getNumber() {
+    var numberChoice = prompt("Please choose a number between 1 - 100");
+    return numberChoice
+}
+
+
+function heatChecker(target, guess) {
+    var difference = Math.abs(target - guess)
+    var direction = target > guess ? "Try higher." : "Try lower."
+    if (difference > 50) {
+        console.log("You're ICE cold. " + direction );
+    } else if (difference > 20 && difference <= 50) {
+        console.log("You're getting warmer. " + direction);
+    } else if (difference > 10 && difference <= 20) {
+        console.log("You're hot. " + direction);
+    } else if (difference > 1 && difference <= 10) {
+        console.log("You're RED hot. " + direction);
+    } else { 
+        console.log("You GOT IT!");
+        return null;
+    } 
+}
+
+
+function guessingGame(userNumber) {
+    var numberTarget = numGenerator();
+    
+    for (var chances = 5; chances >= 1; chances -= 1) {
+        var playerPick = getNumber();
+        
+        if ((typeof playerPick != "number") || (playerPick > 100) || (playerPick < 1)) {
+            console.log("You input was not valid!");
+        } else {
+            heatChecker(numberTarget, playerPick);
+        }
+    }
+}
