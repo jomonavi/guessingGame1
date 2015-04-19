7381065d032032205839e34ff7cbc5d2879d8ef7
Jovi Chokor <jomonavi@gmail.com>
2015-04-18 22:34:24 -0500

    final version 2.0

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ var chances = 5;
 var numberTarget = numGenerator();
 var guessedNumber = []
 
+console.log(numberTarget);
+
 function numGenerator() {
     return Math.floor(Math.random() * 101);
 }
@@ -20,22 +22,25 @@ function repeatNum(array, num) {
 
 
 function heatChecker(target, guess) {
+    // debugger;
     var difference = Math.abs(target - guess)
     var direction = target > guess ? "Try higher." : "Try lower."
 
-    if (difference > 50) {
-        $("feedback").text("You're ICE cold. " + direction );
-    } else if (difference > 20 && difference <= 50) {
+    if (difference >= 50) {
+        $("#feedback").text("You're ICE cold. " + direction );
+    } else if (difference < 50 && difference >= 20) {
         $("#feedback").text("You're getting warmer. " + direction);
-    } else if (difference > 10 && difference <= 20) {
+    } else if (difference < 20  && difference >= 10) {
         $("#feedback").text("You're hot. " + direction);
-    } else if (difference >= 1 && difference <= 10) {
+    } else if (difference < 10 && difference >= 1) {
         $("#feedback").text("You're RED hot. " + direction);
     } else { 
         gameOver = true ;
         $("#feedback").text("You GOT IT!");
+        guessedNumber = [];
         // return null;
     } 
+    chances --;
 }
 
 function userInput() {
@@ -45,29 +50,32 @@ function userInput() {
 
 
 function guessingGame() {
-    if (gameOver) { return null; }
+    if (gameOver) { 
+        $("#feedback").text("Game over! click the 'play again' button to play again!");
+        // return null; 
+    }
         
     var playerPick = userInput();
-    guessedNumber.push(playerPick);
 
     if(chances > 0) {
         
-        if ((typeof playerPick != "number") || (playerPick > 100) || (playerPick < 1)) {
+        if ((typeof playerPick != "number") || (playerPick < 1) || (playerPick > 100)) {
             $("#feedback").text("You input was not valid!");
-        } else if (repeatNum(guessedNumber, playerPick)) {
-            $("#feedback").text("You already tried " + playerPick + "! \n Try another number!");
-        }
-        else { 
+        } else { 
+            if (repeatNum(guessedNumber, playerPick)) {
+                $("#feedback2").text("btw...you already tried " + playerPick + "! \n Try another number!");
+            }
+            debugger;
+            guessedNumber.push(playerPick);
             heatChecker(numberTarget, playerPick);
         }
 
     } else {
             $("#feedback").text("Sorry, you've run out of chances!");
-            guessedNumber.length = 0
-            // return;
+            guessedNumber = [];
+           //reassign entire guesNum variable to empty array
+            return;
         }
-    chances--;
-
 }
 
 $("#hint").click(function() {
